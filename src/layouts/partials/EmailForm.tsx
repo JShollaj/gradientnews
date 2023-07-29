7389bc1b10c2e949175b47e8c7441aedfc13887c
Johnny <xhonisholla@gmail.com>
2023-07-29 19:42:11 +0800

    new form

--- src/layouts/partials/EmailForm.tsx
+++ src/layouts/partials/EmailForm.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 
 const EmailForm = () => {
   const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -26,8 +27,10 @@ const EmailForm = () => {
 
       // Clear the email field
       setEmail('');
+      setMessage('Successfully Subscribed - Check your junk email just in case :)');
     } catch (error) {
       console.error(error);
+      setMessage('An error occurred. Please try again.');
     }
   };
 
@@ -39,14 +42,15 @@ const EmailForm = () => {
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter Email Here"
         required 
-        className="px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-offset-gray-900 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
+        className="px-3 py-2 mb-4 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-offset-gray-900 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
       />
       <button 
         type="submit"
-        className="px-6 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
+        className="px-6 py-2 text-white bg-black rounded-md hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-black"
       >
         Subscribe
       </button>
+      {message && <p className="mt-4 text-green-500">{message}</p>}
     </form>
   );
 };
