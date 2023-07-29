566b9717d158b881fc9b7ba07fd518b5ea6f1e9c
Johnny <xhonisholla@gmail.com>
2023-07-29 17:52:53 +0800

    new form

--- src/layouts/partials/EmailForm.tsx
+++ src/layouts/partials/EmailForm.tsx
@@ -3,52 +3,45 @@ import React, { useState } from "react";
 
 const EmailForm = () => {
   const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    // Replace the URL with your actual API Gateway URL
+    // Use the URL of your API Gateway
     const response = await fetch('https://lged5svcm6.execute-api.ap-southeast-1.amazonaws.com/prod', {
-        method: 'POST',
-        body: JSON.stringify({ email }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-
-    const responseBody = await response.json();
+      method: 'POST',
+      body: JSON.stringify({ email }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
 
     if (!response.ok) {
       // Handle error
       console.error('Failed to subscribe email');
-      setMessage(responseBody.error || 'Failed to subscribe email');
       return;
     }
 
     // Clear the email field
     setEmail('');
-    setMessage('Email stored successfully.');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="mt-4 space-y-4">
       <input 
         type="email" 
         value={email} 
         onChange={(e) => setEmail(e.target.value)}
         placeholder="Enter Email Here"
         required 
-        style={{ marginRight: '10px', padding: '10px', border: '1px solid #ccc', borderRadius: '4px', color: '#000', backgroundColor: '#fff' }} // Inline style
+        className="px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:border-gray-600 dark:focus:ring-offset-gray-900 dark:focus:ring-indigo-500 dark:focus:border-indigo-500"
       />
       <button 
         type="submit"
-        style={{ backgroundColor: '#000', border: 'none', color: '#fff', padding: '10px 24px', textAlign: 'center', textDecoration: 'none', display: 'inline-block', fontSize: '16px', margin: '4px 2px', cursor: 'pointer', borderRadius: '4px' }} // Inline style
+        className="px-6 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
       >
         Subscribe
       </button>
-      {message && <p>{message}</p>}
     </form>
   );
 };
