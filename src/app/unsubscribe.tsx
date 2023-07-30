3f217b4e7ee919e25498bb618dc4b07823c3439a
Johnny <xhonisholla@gmail.com>
2023-07-30 16:45:06 +0800

    New Unsub

--- src/app/unsubscribe.tsx
+++ src/app/unsubscribe.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useCallback } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const EmailForm = () => {
+const UnsubscribeForm = () => {
   const [email, setEmail] = useState("");
   const [recaptchaToken, setRecaptchaToken] = useState<string | null>(null);
   const [message, setMessage] = useState("");
@@ -21,7 +21,7 @@ const EmailForm = () => {
     }
 
     // Use the URL of your API Gateway
-    const response = await fetch('https://lged5svcm6.execute-api.ap-southeast-1.amazonaws.com/prod/subscribe', {
+    const response = await fetch('{Invoke URL}/unsubscribe', {
       method: 'POST',
       body: JSON.stringify({ email, recaptchaToken }),
       headers: {
@@ -31,8 +31,8 @@ const EmailForm = () => {
 
     if (!response.ok) {
       // Handle error
-      setMessage("Failed to subscribe email. Please try again later.");
-      console.error('Failed to subscribe email');
+      setMessage("Failed to unsubscribe email. Please try again later.");
+      console.error('Failed to unsubscribe email');
       return;
     }
 
@@ -41,7 +41,7 @@ const EmailForm = () => {
     setRecaptchaToken(null);
 
     // Display success message
-    setMessage("Successfully subscribed. Check your email every Sunday for latest news!");
+    setMessage("Successfully unsubscribed. You will not receive any more emails from us.");
   };
 
   return (
@@ -64,7 +64,7 @@ const EmailForm = () => {
         type="submit"
         className="px-6 py-2 mt-4 text-white bg-black rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-black"
       >
-        Subscribe
+        Unsubscribe
       </button>
 
       <p>{message}</p>
@@ -72,4 +72,4 @@ const EmailForm = () => {
   );
 };
 
-export default EmailForm;
+export default UnsubscribeForm;
